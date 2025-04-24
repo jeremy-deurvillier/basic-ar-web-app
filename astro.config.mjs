bb0cd6b9dbecd3c72541935f9a77e3685f5cdbd9
jeremy-deurvillier <jerem.deurvil@gmail.com>
2025-04-24 22:26:53 +0200

    Add markers barcode tags and images.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import alpinejs from '@astrojs/alpinejs';
 // https://astro.build/config
 export default defineConfig({
   server: {
-      allowedHosts: ['4cb6-2a01-e0a-4af-52e0-b6bc-425-a514-8af8.ngrok-free.app'],
+      allowedHosts: ['0ab2-2a01-e0a-4af-52e0-6df3-b832-2b32-6932.ngrok-free.app'],
       host: '0.0.0.0',
       port: 3000,
       mimeTypes: {
